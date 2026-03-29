971343607097a800787e501277d6b63c0c4c8ffa
1jar486 <3057059469@qq.com>
2026-03-29 21:22:21 +0800

    再次优化判断逻辑，增加白名单

--- 02-file-practice.py
+++ 02-file-practice.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import shutil
 import sqlite3
 from datetime import datetime
@@ -8,12 +8,13 @@ def init_db(): # 初始化数据库
     # 1.连接数据库。
     # 如果当前文件夹下没有'file_history.db'这个数据库文件，就会自动创建一个新的数据库文件。
     conn = sqlite3.connect('file_history.db') # conn 打开笔记的手
-
+    
     # 2.创建游标
     cursor = conn.cursor() # 创建一个游标对象cursor，所有写字打字的操作都靠它
-  
+
     # 3.编写SQL指令(SQL是专门跟数据库沟通的语言)
     # 下面代码的意思是，如果不存在一个名为 move_logs 的表，就创建一个新的表
+
     # id:自动增长的数字，给每一行一个唯一的编号。
     # file_name:文本类型，记录文件的名字。
     # source_path:文本类型，记录文件原来的位置。
@@ -22,7 +23,7 @@ def init_db(): # 初始化数据库
     cursor.execute('''
                    CREATE TABLE IF NOT EXISTS move_logs (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        file_name TEXT,
+                        file_name TEXT, 
                         source_path TEXT,
                         dest_path TEXT,
                         move_time TEXT
@@ -30,7 +31,7 @@ def init_db(): # 初始化数据库
                    ''') 
     # 4.提交保存
     conn.commit()
-    return conn
+    return conn 
 
 # 这个函数的作用是：往表格里填入一条具体的搬家记录
 # 参数说明：文件名，从哪搬，搬到哪
@@ -73,38 +74,34 @@ def log_move(file_name, source, dest):
 
 # 主逻辑开始
 def main():
-    init_db()
+    init_db() # 初始化数据库
     current_path = os.path.dirname(os.path.realpath(__file__)) # 获取当前路径，让脚本自动识别自己在哪个文件路径
     print(f"我现在的办公地点是：{current_path}")
     files = os.listdir(current_path)
     
+    WHITE_LIST_EXTS = ['.JPG','.PNG','.PDF','.DOCX','.MD','.MP4','.ZIP']
     
-    for file in os.listdir(current_path):
-        if os.path.isfile(file) and file!= os.path.basename(__file__):
-        # 如果这个文件是当前正在运行的脚本文件，就跳过它，不管它
-            if file == os.path.basename(__file__):
-               continue
-        # os.path.join()拼接完整路径，也就是获取这个文件夹里面的文件或文件夹的绝对地址
+    for file in os.listdir(current_path): # 扫描这个文件路径下的所有文件
         full_path = os.path.join(current_path, file)
-       # 1. 排除系统隐藏文件
+        # os.path.isfile(file) 判断这个东西是不是文件
+        # file != os.path.basename(__file__) 判断这个东西是不是脚本本身
+        
+        if not os.path.isfile(full_path):
+            print(f"跳过文件夹":{file})
+            continue
+        
+       # 1. if or file.startswith('.') 排除系统隐藏文件
         if file.upper() in ["NTUSER.DAT", "INDEX.DAT"] or file.startswith('.'):
             print(f"👻 跳过隐藏/系统文件: {file}")
             continue
         
-       # 2. 排除你的 Python 脚本和数据库账本
-        # 增加 file == "file_history.db" 这个条件
-        if file == os.path.basename(__file__) or file == "02_file_logger.py" or file == "file_history.db":
-            print(f"🛠️ 跳过代码脚本/数据库: {file}")
-            continue
-            
-        full_path = os.path.join(current_path, file)
-        
-        # 3. 排除已经建好的分类文件夹（关键修改，这句打印之前误导了你）
-        if not os.path.isfile(full_path):
-            print(f"📁 发现文件夹，不作处理: {file}")
-            continue
-        
         name, ext = os.path.splitext(file)
+        ext_upper = ext.upper() # 获取文件后缀名大写
+
+        if ext_upper not in WHITE_LIST_EXTS:
+            print(f"跳过非目标文件")
+            continue
+        
         # 把后缀名的点去掉([1:])，并转成大写(upper())，作为文件夹的名字
         folder_name = ext[1:].upper() 
          # 双重拼接，先把文件夹路径和文件夹名字粘合成完整路径，再把这个路径和文件名粘合成新文件的完整路径
@@ -131,7 +128,7 @@ def main():
 
         log_move(file, old_file_path, target_file_path )
 
-if __name__ == "__main__":
+if __name__ == "__main__": # "__main__" 两段_ 防止出现语法错误
     main()
             
 
